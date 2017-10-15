412041b1287785f179d0be663f2bf6d114c179a5
DESKTOP-4INFFS5\Angela <angcai@outlook.com>
2017-10-15 04:41:05 +0100

    15102017_A
    Started learning C++;
    Cloned Andy's Unity Examples.

---
+++ C++/0001_HelloWorld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout<<"Hellp World!";
+   return 0;
+}
---
+++ C++/0002_BlankSpace.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   cout<<"I'm learning \n";
+   cout<<"how to" <<endl;
+   cout<<"code!";
+   return 0;
+}
